c010da20b448c63f38ac7c18b84885aef3259184
Oleg Sevostyanov <savosarenskyrim@gmail.com>
2021-12-25 16:24:07 +0500

    test commint

--- MainConsoleApp/Program.cs
+++ MainConsoleApp/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace MainConsoleApp
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace MainConsoleApp
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            var a = 10;
+            Console.WriteLine(a);
         }
     }
 }
