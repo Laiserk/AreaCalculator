7e449c1f9b97d007c10f78e787ee79fd1732fc74
Oleg Sevostyanov <savosarenskyrim@gmail.com>
2021-12-26 21:52:23 +0500

    implementation of the design pattern Command

---
+++ AreaCalculatorLib/Commands/CalculatingAreaCommand.cs
@@ -0,0 +1,17 @@
+﻿using AreaCalculatorLib.Figures;
+
+namespace AreaCalculatorLib.Commands
+{
+    public class CalculatingAreaCommand : Command
+    {
+        private readonly IFigure _figure;
+        public CalculatingAreaCommand(IFigure figure)
+        {
+            _figure = figure;
+        }
+        public override void ExecuteCommand()
+        {
+            Result = _figure.Area();
+        }
+    }
+}
---
+++ AreaCalculatorLib/Commands/Command.cs
@@ -0,0 +1,10 @@
+﻿using AreaCalculatorLib.Interfaces;
+
+namespace AreaCalculatorLib.Commands
+{
+    public abstract class Command : ICommand
+    {
+        public double Result { get; set; }
+        public abstract void ExecuteCommand();
+    }
+}
